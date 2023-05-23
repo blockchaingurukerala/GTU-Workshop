8990f0933769396fb1cd7f840bc73ef1f448a612
blockchaingurukerala <blockchaingurukerala@gmail.com>
2023-05-23 16:39:12 +0530

    first coomit

---
+++ contracts/Student.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+contract Student{
+    string public name="Prince";
+    function updateName(string memory _n) public {
+        name=_n;
+    }
+}
